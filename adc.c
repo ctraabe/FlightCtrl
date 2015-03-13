b30973d352ccbbeae6eaeadad8dee0794dbbeaed
Chris Raabe <chris@raabe.nospam>
2015-03-12 20:44:06 -0600

    Revised the approach fro EEPROM to maintain the address of previous elements even after adding new ones. Added checks on loaded data. Modified some names.

--- adc.c
+++ adc.c
@@ -1,11 +1,17 @@
 #include "adc.h"
 
+#include <stdlib.h>
+#include <avr/eeprom.h>
 #include <avr/io.h>
 #include <util/atomic.h>
+
+#include "eeprom.h"
 
 
 // =============================================================================
 // Private data:
+
+#define ADC_MIDDLE_VALUE (1023 / 2)
 
 // ADC sample indices
 enum ADCSensorIndex {
@@ -21,17 +27,20 @@ enum ADCSensorIndex {
 
 // The following are not declared static so that they will be visible to adc.S.
 volatile uint16_t samples_[ADC_N_SAMPLES][ADC_N_CHANNELS];
-volatile uint8_t samples_index_ = 0;
+volatile uint8_t samples_index_;
 
-static uint16_t baro_altitude_, battery_voltage_;
+static uint16_t biased_pressure_, battery_voltage_;
 static int16_t acceleration_[3], angular_rate_[3];
-static int16_t acc_offset_[3], gyro_offset_[3];
+static int16_t acc_offset_[3];
+static int16_t gyro_offset_[3] = { ADC_MIDDLE_VALUE * ADC_N_SAMPLES,
+  ADC_MIDDLE_VALUE * ADC_N_SAMPLES, ADC_MIDDLE_VALUE * ADC_N_SAMPLES };
 
 
 // =============================================================================
 // Private function declarations:
 
 static inline uint16_t ADCSample(enum ADCSensorIndex sensor);
+static void CheckOffset(int16_t offset[3], int16_t acceptable_deviation);
 static inline uint16_t SumRecords(enum ADCSensorIndex sensor);
 
 
@@ -75,15 +84,21 @@ int16_t AngularRate(enum SensorAxes axis)
 }
 
 // -----------------------------------------------------------------------------
-uint16_t BaroAltitude(void)
-{
-  return baro_altitude_;
-}
-
-// -----------------------------------------------------------------------------
 uint16_t BatteryVoltage(void)
 {
   return battery_voltage_;
+}
+
+// -----------------------------------------------------------------------------
+uint16_t BiasedPressure(void)
+{
+  return biased_pressure_;
+}
+
+// -----------------------------------------------------------------------------
+uint16_t BiasedPressureSensor(void)
+{
+  return ADCSample(ADC_PRESSURE);
 }
 
 // -----------------------------------------------------------------------------
@@ -102,12 +117,6 @@ uint16_t Gyro(enum SensorAxes axis)
     default:
       return ADCSample(ADC_GYRO_Z);
   }
-}
-
-// -----------------------------------------------------------------------------
-uint16_t PressureSensor(void)
-{
-  return ADCSample(ADC_PRESSURE);
 }
 
 
@@ -141,25 +150,127 @@ void ProcessSensorReadings(void)
 {
   acceleration_[X_AXIS] = -SumRecords(ADC_ACCEL_X) - acc_offset_[X_AXIS];
   acceleration_[Y_AXIS] = -SumRecords(ADC_ACCEL_Y) - acc_offset_[Y_AXIS];
-  acceleration_[Z_AXIS] = SumRecords(ADC_ACCEL_Z) - acc_offset_[Z_AXIS];
+  acceleration_[Z_AXIS] = -SumRecords(ADC_ACCEL_Z) - acc_offset_[Z_AXIS];
 
   angular_rate_[X_AXIS] = -SumRecords(ADC_GYRO_X) - gyro_offset_[X_AXIS];
   angular_rate_[Y_AXIS] = -SumRecords(ADC_GYRO_Y) - gyro_offset_[Y_AXIS];
-  angular_rate_[Z_AXIS] = -SumRecords(ADC_GYRO_Z) - gyro_offset_[Z_AXIS];
+  angular_rate_[Z_AXIS] = SumRecords(ADC_GYRO_Z) - gyro_offset_[Z_AXIS];
 
-  baro_altitude_ = 1024 * ADC_N_SAMPLES - SumRecords(ADC_PRESSURE);
+  biased_pressure_ = SumRecords(ADC_PRESSURE);
 
   battery_voltage_ = SumRecords(ADC_BATT_V);
 }
 
 // -----------------------------------------------------------------------------
 // This function delays program execution until the ADC sample array has been
-// fully refreshed at least once.
+// fully refreshed.
 void WaitOneADCCycle(void)
 {
   if (!(ADCSRA & _BV(ADSC))) return;  // ADC not running.
-  while (!samples_index_) continue;
-  while (!samples_index_) continue;
+  uint8_t samples_index_tmp = samples_index_;
+  while (samples_index_tmp == samples_index_) continue;
+  while (samples_index_tmp != samples_index_) continue;
+}
+
+// -----------------------------------------------------------------------------
+// This function assumes that the Mikrokopter is motionless on the ground. It
+// finds the average gyro readings over the period of 1 second (approximately)
+// and considers the results to be the zero values of the gyros.
+void ZeroGyros(void)
+{
+  uint32_t gyro_sum[3] = { 0 };
+
+  // TODO: Never block motor communication when running.
+  // if (MotorsOn()) return 1;
+
+  // Clear offsets.
+  gyro_offset_[X_AXIS] = 0;
+  gyro_offset_[Y_AXIS] = 0;
+  gyro_offset_[Z_AXIS] = 0;
+
+  // Sum samples over about 1 second (2000 samples).
+  // Note: the number of samples must be evenly divisible by ADC_N_SAMPLES.
+  const uint16_t kNSamples = 2000 / ADC_N_SAMPLES;
+  for (uint8_t i = 0; i < kNSamples; i++)
+  {
+    WaitOneADCCycle();
+    ProcessSensorReadings();
+    gyro_sum[X_AXIS] += (uint16_t)-angular_rate_[X_AXIS];
+    gyro_sum[Y_AXIS] += (uint16_t)-angular_rate_[Y_AXIS];
+    gyro_sum[Z_AXIS] += (uint16_t)angular_rate_[Z_AXIS];
+  }
+
+  // Average the results and set as the offset.
+  gyro_offset_[X_AXIS] = (int16_t)(gyro_sum[X_AXIS] / kNSamples);
+  gyro_offset_[Y_AXIS] = (int16_t)(gyro_sum[Y_AXIS] / kNSamples);
+  gyro_offset_[Z_AXIS] = (int16_t)(gyro_sum[Z_AXIS] / kNSamples);
+
+  // TODO: Change these limits to something more reasonable.
+  // Check that the zero values are within an acceptable range. The acceptable
+  // range is specified in ADC steps from the ADC middle value (511).
+  const uint16_t kAcceptableDeviation = 20;
+  CheckOffset(gyro_offset_, kAcceptableDeviation);
+}
+
+// -----------------------------------------------------------------------------
+// This function assumes that the Mikrokopter is motionless (on the ground). It
+// finds the average accelerometer readings over the period of 1 second
+// (approximately) and considers the results to be the zero values of the
+// accelerometers.
+void ZeroAccelerometers(void)
+{
+  uint32_t acc_sum[3] = {0};
+
+  // TODO: Never block motor communication when running.
+  // if (MotorsOn()) return 1;
+
+  // Clear offsets.
+  acc_offset_[X_AXIS] = 0;
+  acc_offset_[Y_AXIS] = 0;
+  acc_offset_[Z_AXIS] = 0;
+
+  // Sum samples over about 1 second (2000 samples).
+  // Note: the number of samples must be evenly divisible by ADC_N_SAMPLES.
+  const uint16_t kNSamples = 2000 / ADC_N_SAMPLES;
+  for (uint8_t i = 0; i < kNSamples; i++)
+  {
+    WaitOneADCCycle();
+    ProcessSensorReadings();
+    acc_sum[X_AXIS] += (uint16_t)-acceleration_[X_AXIS];
+    acc_sum[Y_AXIS] += (uint16_t)-acceleration_[Y_AXIS];
+    acc_sum[Z_AXIS] += (uint16_t)-acceleration_[Z_AXIS];
+  }
+
+  // Average the results and set as the offset.
+  acc_offset_[X_AXIS] = (int16_t)(acc_sum[X_AXIS] / kNSamples);
+  acc_offset_[Y_AXIS] = (int16_t)(acc_sum[Y_AXIS] / kNSamples);
+  acc_offset_[Z_AXIS] = (int16_t)(acc_sum[Z_AXIS] / kNSamples);
+
+  // TODO: Change these limits to something more reasonable
+  // Check that the zero values are within an acceptable range. The acceptable
+  // range is specified in ADC steps from the ADC middle value (511).
+  const uint16_t kAcceptableDeviation = 20;
+  CheckOffset(acc_offset_, kAcceptableDeviation);
+
+  // Save the values in the EEPROM.
+  // TODO: Make this contingent on success of CheckOffset
+  eeprom_update_block((const void*)acc_offset_, (void*)&eeprom.acc_offset[0],
+    sizeof(acc_offset_));
+}
+
+// -----------------------------------------------------------------------------
+// This function loads the accelerometer offsets from EEPROM so that
+// accelerometers don't have to be re-calibrated every flight.
+void LoadAccelerometerOffsets(void)
+{
+  eeprom_read_block((void*)acc_offset_, (const void*)&eeprom.acc_offset[0],
+    sizeof(acc_offset_));
+
+  // TODO: Change these limits to something more reasonable
+  // Check that the zero values are within an acceptable range. The acceptable
+  // range is specified in ADC steps from the ADC middle value (511).
+  const uint16_t kAcceptableDeviation = 20;
+  CheckOffset(acc_offset_, kAcceptableDeviation);
 }
 
 
@@ -170,6 +281,20 @@ void WaitOneADCCycle(void)
 static inline uint16_t ADCSample(enum ADCSensorIndex sensor)
 {
   return samples_[(samples_index_ - sensor) / ADC_N_CHANNELS][sensor];
+}
+
+// -----------------------------------------------------------------------------
+// This function checks the calculated neutral value against predetermined
+// limits and reports an error if a limit is exceeded.
+static void CheckOffset(int16_t offset[3], int16_t acceptable_deviation)
+{
+  for (int i = 0; i < 3; i++)
+  {
+    int16_t deviation = abs(offset[i] / ADC_N_SAMPLES - ADC_MIDDLE_VALUE);
+    if (deviation > acceptable_deviation)
+      // TODO: set an error
+      return;
+  }
 }
 
 // -----------------------------------------------------------------------------
---
+++ eeprom.h
@@ -0,0 +1,27 @@
+// This file provides a structure and initial conditions for placing persistent
+// variables in EEPROM. This method ensures that variables remain in the same
+// memory location every for every compilation. This means that EEPROM doesn't
+// have to be cleared when the chip is reprogrammed, and previously stored
+// values will remain even after programming.
+
+#ifndef EEPROM_H_
+#define EEPROM_H_
+
+
+#include <inttypes.h>
+#include <avr/eeprom.h>
+
+
+struct EEPROM
+{
+  uint8_t space[2014];  // 2kB space reserved for original Mikrokopter firmware.
+  int16_t acc_offset[3];
+  uint8_t pressure_coarse_bias;
+  uint16_t coarse_bias_steps_to_pressure_steps;
+  uint16_t fine_bias_steps_to_pressure_steps;
+};
+
+extern struct EEPROM eeprom;
+
+
+#endif  // EEPROM_H_
