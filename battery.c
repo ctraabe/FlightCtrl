6388bec0d7b9291b4ca31e195603907d37aff783
Chris Raabe <chris@raabe.nospam>
2015-05-01 17:32:40 +0900

    Reduced the number of detectable cells from 6 to 4.

--- battery.c
+++ battery.c
@@ -9,7 +9,7 @@
 // =============================================================================
 // Private data:
 
-static uint8_t voltage_low_limit_ = 231;
+static uint8_t voltage_low_limit_ = 255;
 // static uint16_t current_estimate_ = 0, power_estimate_ = 0, used_capactiy_ = 0;
 // static uint16_t update_timer_ = 0;
 
@@ -17,6 +17,8 @@ static uint8_t voltage_low_limit_ = 231;
 // =============================================================================
 // Public functions:
 
+// Detects the number of cells for the attached battery up to 4 cells (it is
+// impossible to detect the number of cells for batteries with 5 or more cells).
 void DetectBattery(void)
 {
   if (ADCState() == ADC_INACTIVE) return;
@@ -29,10 +31,10 @@ void DetectBattery(void)
   uint8_t n_cells;
   const uint8_t kMinVoltsPerCell = 33;  // 3.3 V is the LiPo operational minimum
   const uint8_t kMaxVoltsPerCell = 43;  // 4.3 V is max for LiPo batteries
-  for (n_cells = 2; (n_cells < 7) && (BatteryVoltage() > n_cells
+  for (n_cells = 2; (n_cells < 5) && (BatteryVoltage() > n_cells
     * kMaxVoltsPerCell); n_cells++) continue;
 
-  voltage_low_limit_ = n_cells * kMinVoltsPerCell;
+  if (n_cells < 5) voltage_low_limit_ = n_cells * kMinVoltsPerCell;
   BeepNTimes(n_cells, 200);
 }
 
