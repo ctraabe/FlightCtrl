fc6e22a3ec59ff981a70d0d333390001ef84a33b
Chris Raabe <chris@raabe.nospam>
2015-12-01 18:33:39 +0900

    Formatting (non-functional)

--- mcu_pins.h
+++ mcu_pins.h
@@ -28,19 +28,19 @@
 #define BUZZER_PORT (PORTC)
 #define BUZZER_PIN (_BV(7))
 
-// LED
+// LEDs
 #define LED_DDR (DDRB)
 #define LED_PORT (PORTB)
 #define GREEN_LED_PIN (_BV(1))
 #define RED_LED_PIN (_BV(0))
 
-// EXTERNAL LEDS
+// External LEDs
 #define EXTERNAL_LED_DDR (DDRC)
 #define EXTERNAL_LED_PORT (PORTC)
 #define EXTERNAL_LED_1_PIN (_BV(2))
 #define EXTERNAL_LED_3_PIN (_BV(3))
 
-// I2C (MOTORS)
+// I2C (motors)
 #define I2C_DDR (DDRC)
 #define I2C_PORT (PORTC)
 #define I2C_SCL_PIN (_BV(0))
