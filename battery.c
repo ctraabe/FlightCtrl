7d2393867a3ca746334c3cc1057faf27bf59b357
Chris Raabe <chris@raabe.nospam>
2016-12-06 00:32:42 +0900

    Modified battery detection logic... again

--- battery.c
+++ battery.c
@@ -2,15 +2,13 @@
 
 #include "adc.h"
 #include "buzzer.h"
-#include "motors.h"
+#include "state.h"
 
 
 // =============================================================================
 // Private data:
 
 static uint8_t voltage_low_limit_ = 255;
-// static uint16_t current_estimate_ = 0, power_estimate_ = 0, used_capactiy_ = 0;
-// static uint16_t update_timer_ = 0;
 
 
 // =============================================================================
@@ -20,20 +18,22 @@ static uint8_t voltage_low_limit_ = 255;
 // impossible to detect the number of cells for batteries with 5 or more cells).
 void DetectBattery(void)
 {
-  if (ADCState() == ADC_INACTIVE) return;
-
-  // TODO: if (motors_on) return
+  if (MotorsRunning() || (ADCState() == ADC_INACTIVE)) return;
 
   WaitOneADCCycle();
   ProcessSensorReadings();
 
-  uint8_t n_cells;
+  uint8_t n_cells = 0;
   const uint8_t kMinVoltsPerCell = 33;  // 3.3 V is the LiPo operational minimum
   const uint8_t kMaxVoltsPerCell = 43;  // 4.3 V is max for LiPo batteries
-  for (n_cells = 2; (n_cells < 5) && (BatteryVoltage() > (n_cells
-    * kMaxVoltsPerCell - 2U)); n_cells++) continue;  // 0.2 V margin
 
-  if (n_cells < 5) voltage_low_limit_ = n_cells * kMinVoltsPerCell;
+  // Ignore battery checks if not being powered by a battery.
+  if (BatteryVoltage() < kMinVoltsPerCell) return;
+
+  while ((n_cells < 4) && (BatteryVoltage() > (n_cells * kMaxVoltsPerCell)))
+    n_cells++;
+  voltage_low_limit_ = n_cells * kMinVoltsPerCell;
+
   BeepNTimes(n_cells, 200);
   WaitForBuzzerToComplete();
 }
